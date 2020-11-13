678340090f312a987353ce2d6a75b8be88260d75
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-11-13 13:04:24 +0100

    Add: *RenderPipeLines *Some usefull art for placeholders *Instantiante a object, on the mouse position

---
+++ HeroDefender/Assets/BuildingMenager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMenager : MonoBehaviour
+{
+    [SerializeField] private Transform WoodHarvester;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(WoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = (mainCamera.ScreenToWorldPoint(Input.mousePosition));
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
