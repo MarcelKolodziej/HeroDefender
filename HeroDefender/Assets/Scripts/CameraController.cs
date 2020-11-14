1e8251d1d941c5b2413cc740889053e3aa087a57
Elliot <elliotgriffiss@gmail.com>
2020-11-14 04:07:57 +0000

    Imported the Tilemap Extras
    - Started Implementing some other basic features
    - Added a camera controller so it moves on mouse pointer drag
    - Added some code to help debug raycasts
    - Added some tags and layers

---
+++ HeroDefender/Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera MainCamera;
+    [SerializeField] private BoxCollider2D CameraRestrictor;
+    [SerializeField] private float DragSpeed = 1f;
+
+    private Vector2 dragOrigin;
+    private Vector2 newPosition;
+    private Vector2 moveAmount;
+
+    private void Start()
+    {
+        Debug.Log(CameraRestrictor.size.x);
+    }
+
+    public void OnPointerDown(BaseEventData data)
+    {
+        dragOrigin = data.currentInputModule.input.mousePosition;
+        Debug.Log("On Mouse Down: " + dragOrigin);
+    }
+
+    public void OnDrag(BaseEventData data)
+    {
+        newPosition = MainCamera.ScreenToViewportPoint(data.currentInputModule.input.mousePosition - dragOrigin);
+        Debug.Log("On Mouse Drag: " + newPosition);
+
+        moveAmount = new Vector2(-newPosition.x * DragSpeed, -newPosition.y * DragSpeed);
+
+        if (newPosition.x > (CameraRestrictor.size.x / 2))
+        {
+            moveAmount.x = CameraRestrictor.size.x / 2;
+        }
+
+        if (newPosition.x < -(CameraRestrictor.size.x / 2))
+        {
+            moveAmount.x = -(CameraRestrictor.size.x / 2);
+        }
+
+        if (newPosition.y > (CameraRestrictor.size.y / 2))
+        {
+            moveAmount.y = CameraRestrictor.size.y / 2;
+        }
+
+        if (newPosition.y < -(CameraRestrictor.size.y / 2))
+        {
+            moveAmount.y = -(CameraRestrictor.size.x / 2);
+        }
+
+        MainCamera.transform.Translate(moveAmount, Space.World);
+    }
+
+    private void OnMouseUp()
+    {
+    }
+}
---
+++ HeroDefender/Assets/Scripts/Debug/RaycastDebugger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RaycastDebugger : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Name = " + hit.collider.name);
+                Debug.Log("Tag = " + hit.collider.tag);
+                Debug.Log("Hit Point = " + hit.point);
+                Debug.Log("Object position = " + hit.collider.gameObject.transform.position);
+                Debug.Log("--------------");
+            }
+            else
+            {
+                Debug.Log("No Rays Found");
+            }
+        }
+    }
+}
