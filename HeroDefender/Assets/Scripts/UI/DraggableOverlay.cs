f7c987e4bda3ae80daf55f225be0fb2654814a45
Elliot <elliotgriffiss@gmail.com>
2020-11-18 22:48:39 +0000

    SImple Bug Fix For Building Placement

--- HeroDefender/Assets/Scripts/UI/DraggableOverlay.cs
+++ HeroDefender/Assets/Scripts/UI/DraggableOverlay.cs
@@ -7,12 +7,14 @@ public class DraggableOverlay : MonoBehaviour
     public SpriteRenderer PlacementOverlay;
     public LayerMask PlacementLayermask;
     public bool IsOverlapping;
+    private int EnemiesInsideCollisionRange = 0;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer != PlacementLayermask)
         {
             IsOverlapping = true;
+            EnemiesInsideCollisionRange++;
             PlacementOverlay.color = Color.red;
         }
     }
@@ -21,8 +23,13 @@ public class DraggableOverlay : MonoBehaviour
     {
         if (collision.gameObject.layer != PlacementLayermask)
         {
-            IsOverlapping = false;
-            PlacementOverlay.color = Color.green;
+            EnemiesInsideCollisionRange--;
+
+            if (EnemiesInsideCollisionRange < 1)
+            {
+                IsOverlapping = false;
+                PlacementOverlay.color = Color.green;
+            }
         }
     }
 }
